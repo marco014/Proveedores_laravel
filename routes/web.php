ab8b49f47d8a70b6a93e6edb33c3dee60bcd2474
marcoD <marcodarineldiaz@gmail.com>
2025-11-06 13:39:41 -0600

    fix: Corregir el enlace del modelo en las rutas de proveedores

--- routes/web.php
+++ routes/web.php
@@ -27,8 +27,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    // Rutas para el recurso Proveedor
+    Route::resource('proveedores', ProveedorController::class)->parameters(['proveedores' => 'proveedor']);
 });
 
-Route::resource('proveedores', ProveedorController::class)->middleware(['auth']);
 
 require __DIR__.'/auth.php';
